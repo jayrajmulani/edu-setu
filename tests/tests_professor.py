87162ae29d987fa6ea3872d0eb5c6281f256e8ad
Harshil Sanghavi <harshilsan7@gmail.com>
2022-10-09 15:15:48 -0400

    test cases professor api

--- tests/tests_professor.py
+++ tests/tests_professor.py
@@ -1,18 +1,36 @@
 import sys
 import traceback
-
+from flask import Flask
+import json
 
 sys.path.append("../code/backend")
 import utils
 import professor_api 
 
-def test_get_all_postings_by_professor():
-    data = { "professor":1010 }
-    x = professor_api.get_all_postings_by_professor(data)
-    print(x)
-    if x :
-        print('True')
-    else:
-        print('False')
+def test_index_route():
+    response = professor_api.test_client().get('http://140.238.250.0:')
+
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Testing, Flask!'
     
-test_get_all_postings_by_professor()
+def test_get_all_postings():
+    response = professor_api.test_client().get('/get_all_postings')
+    res = json.loads(response.data.decode('utf-8')).get("data")
+    assert type(res[0]) is dict
+    #assert res[0]['author'] == 'Havard'
+   
+    assert response.status_code == 200
+    #assert type(res) is list
+
+# def test_get_all_postings_by_professor():
+#     data = { "professor":1010 }
+#     x = professor_api.get_all_postings_by_professor(data)
+#     print(x)
+#     if x :
+#         print('True')
+#     else:
+#         print('False')
+    
+# test_get_all_postings_by_professor()
+
+
