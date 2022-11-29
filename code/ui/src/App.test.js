fff7738e30b712c4fbbfbc4405fa1c66e4e0711a
Ekansh Singhal <ekanshsinghal@icloud.com>
2022-11-28 20:45:27 -0500

    fixed test case workflow

--- code/ui/src/App.test.js
+++ code/ui/src/App.test.js
@@ -2,8 +2,18 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("should render Login component", () => {
+	Object.defineProperty(window, "matchMedia", {
+		writable: true,
+		value: jest.fn().mockImplementation((query) => ({
+			matches: false,
+			media: query,
+			onchange: null,
+			addListener: jest.fn(), // Deprecated
+			removeListener: jest.fn(), // Deprecated
+			addEventListener: jest.fn(),
+			removeEventListener: jest.fn(),
+			dispatchEvent: jest.fn(),
+		})),
+	});
 	render(<App />);
-	const Element = screen.getByText(/EDU-SETU/i);
-	expect(Element).toBeInTheDocument();
-	// expect(LoginElement).toHaveTextContent('');
 });
