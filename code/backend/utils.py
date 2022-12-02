662e6230364283ea63dfce5392b64e52b24fc350
Rahul RK <rahulrk.2303@gmail.com>
2022-12-02 07:13:49 -0500

    Fixing all OS update utils

--- code/backend/utils.py
+++ code/backend/utils.py
@@ -1,4 +1,5 @@
 import cx_Oracle
+import sys
 from config import *
 
 
@@ -9,8 +10,19 @@ def makeDictFactory(cursor):
         return dict(zip(columnNames, args))
     return createRow
 
-
 def connect():
+
+    try:
+        if sys.platform.startswith("darwin"):
+            cx_Oracle.init_oracle_client(lib_dir="instantclient_19_8") # mac - Change it to the directory where you have this folder.
+        elif sys.platform.startswith("win32"):
+            cx_Oracle.init_oracle_client(lib_dir="instantclient_21_7")
+        elif sys.platform.startswith("linux"):
+            cx_Oracle.init_oracle_client(lib_dir="instantclient_21_8")
+        else:
+            cx_Oracle.init_oracle_client()
+    except Exception as e:
+        con = None
     try:
         con = cx_Oracle.connect(db_user, db_pass, db_conn)
     except Exception as e:
