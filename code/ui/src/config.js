3386de85f55ae5b24f521c31ffae5510f1d0c3fc
Supriya Krishna <sbkrishn@ncsu.edu>
2022-12-03 14:24:33 -0500

    URL modified

--- code/ui/src/config.js
+++ code/ui/src/config.js
@@ -1,5 +1,5 @@
 const config = {
 	// baseUrl: "http://127.0.0.1:5000", // For development
-	baseUrl: "http://se-backend-load-balancer-984343860.us-east-1.elb.amazonaws.com/", // Connected to AWS backend
+	baseUrl: "http://se-backend-load-balancer-984343860.us-east-1.elb.amazonaws.com", // Connected to AWS backend
 };
 export default config;
