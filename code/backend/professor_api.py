6319f54d1e638692a66d214dfd46567d41560661
Rahul <rahul@Rahuls-MacBook-Pro.local>
2022-12-03 04:54:44 -0500

    added default content for email

--- code/backend/professor_api.py
+++ code/backend/professor_api.py
@@ -484,7 +484,8 @@ def send_email(data):
         to: string (student email address), 
         professor_name: string (professor name),
         subject: string (email subject),
-        message: string (email content)
+        message: string (email content),
+        posting_id: number (job posting id)
     }
     Response:
     {
@@ -497,9 +498,19 @@ def send_email(data):
     try:
         # Get the data from JSON Payload
         to = data["to"]
-        professor_name = data["professor_name"]
-        subject = data["subject"]
-        message = "From " + professor_name + "\n\n" + data["message"]
+        posting_id = data["posting_id"]
+        if "professor_name" in data:
+            professor_name = data["professor_name"]
+        else:
+            professor_name = "Professor"
+        if "subject" in data:
+            subject = data["subject"]
+        else:
+            subject = "Your application status has been updated"
+        if "message" in data:
+            message = "From " + professor_name + "\n\n" + data["message"] + "\n\Posting ID: " + str(posting_id)
+        else:
+            message = "Unfortunately, you have not been selected for " + str(posting_id) + ". Thank you!"
 
         email_address = os.environ["SMTP_EMAIL"]
         email_password = os.environ["SMTP_PASSWORD"]
